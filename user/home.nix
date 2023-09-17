ddc124506637f44a87fb22f62d04cc1229146444
G.Getsov <G.Getsov.Dev@gmail.com>
2023-09-17 10:48:41 +0300

    Gnome fonts

--- user/home.nix
+++ user/home.nix
@@ -114,6 +114,16 @@ let
       "org/gnome/desktop/wm/preferences" = {
         button-layout = "appmenu:minimize,maximize,close";
       };
+
+      "org/gnome/desktop/interface" = {
+        font-name = "JetBrainsMono Nerd Font 14";
+        document-font-name = "JetBrainsMono Nerd Font 14";
+        monospace-font-name = "JetBrainsMono Nerd Font 14";
+      };
+
+      "org/gnome/desktop/wm/preferences" = {
+        titlebar-font = "JetBrainsMono Nerd Font 14";
+      };
     };
   };
   qt = {
