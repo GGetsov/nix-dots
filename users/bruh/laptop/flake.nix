0b9dbf700a923711d1c5f6ce898a3cee5fd37f25
GGetsov <g.getsov.dev@gmail.com>
2023-10-01 10:34:29 +0300

    Home-manager flake

---
+++ users/bruh/laptop/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Home manager laptop config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+  };
+
+  outputs = { nixpkgs, home-manager, neovim-nightly-overlay, ... }@inputs:
+  let 
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+
+    overlays = [
+      inputs.neovim-nightly-overlay.overlay
+    ];
+
+  in {
+    homeConfigurations = {
+      bruh = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        # inherit overlays;
+
+        modules = [
+          ({nixpkgs.overlays = overlays;})
+          ../src/home.nix
+          ];
+      }; 
+    };
+  };
+}
