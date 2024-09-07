5ca25a779485624d32f42481e00c1d432e8d41c2
GGetsov <g.getsov.dev@gmail.com>
2024-09-07 09:32:00 +0300

    Configure Zsh

--- users/bruh/src/home.nix
+++ users/bruh/src/home.nix
@@ -22,21 +22,22 @@ in
     enable = true;
   };
 
-  # programs.zsh = {
-  #   enable = true;
-  #   enableCompletion = true;
-  #   autosuggestion.enable = true;
-  #   syntaxHighlighting.enable = true;
-  #
-  #   shellAliases = {
-  #     ll = "ls -l";
-  #     update = "sudo nixos-rebuild switch";
-  #   };
-  #   history = {
-  #     size = 10000;
-  #     path = "${config.xdg.dataHome}/zsh/history";
-  #   };
-  # };
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+    shellAliases = {
+      ll = "ls -l";
+    };
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+  };
 
   home.sessionVariables = {
     EDITOR = "nvim";
